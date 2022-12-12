38f8159f4ef65715b2e2d5c42fa024f2189cc443
Abhishek Kumaar <39941327+abhishekkkumaar@users.noreply.github.com>
2022-12-12 18:08:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let container = document.getElementById('count');                       
+        setTimeout(function(){
+        container.innerHTML = "10";
+    setTimeout(function(){
+        container.innerHTML = "9";
+    setTimeout(function(){
+        container.innerHTML="8";
+    setTimeout(function(){
+        container.innerHTML="7";
+    setTimeout(function(){
+        container.innerHTML="6";
+    setTimeout(function(){
+        container.innerHTML="5";
+    setTimeout(function(){
+        container.innerHTML="4";
+    setTimeout(function(){
+        container.innerHTML="3";
+    setTimeout(function(){
+        container.innerHTML="2";
+    setTimeout(function(){
+        container.innerHTML="1";
+setTimeout(function(){
+    container.innerHTML="Happy Independence Day!!!";
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+    },1000);
+   },1000);
+},1000);
+
+
+/* setTimeout(()=>{
+    document.querySelector(".container").innerHTML="<h1>10</h1>";
+    setTimeout(()=>{
+        document.querySelector(".container").innerHTML="<h1>9</h1>";
+        setTimeout(()=>{
+            document.querySelector(".container").innerHTML="<h1>8</h1>";
+            setTimeout(()=>{
+                document.querySelector(".container").innerHTML="<h1>7</h1>";
+                setTimeout(()=>{
+                    document.querySelector(".container").innerHTML="<h1>6</h1>";
+                    setTimeout(()=>{
+                        document.querySelector(".container").innerHTML="<h1>5</h1>";
+                        setTimeout(()=>{
+                            document.querySelector(".container").innerHTML="<h1>4</h1>";
+                            setTimeout(()=>{
+                                document.querySelector(".container").innerHTML="<h1>3</h1>";
+                                setTimeout(()=>{
+                                    document.querySelector(".container").innerHTML="<h1>2</h1>";
+                                    setTimeout(()=>{
+                                        document.querySelector(".container").innerHTML="<h1>1</h1>";
+},2000);
+    },2000);
+            },2000);
+                },2000);
+                    },2000);
+                        },2000);
+                            },2000);
+                                },2000);
+                                    },2000);
+                                        },2000);
+
+ */     
+    
